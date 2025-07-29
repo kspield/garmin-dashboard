431fe7b90260805f59dfd10f514bf503ae4a8e2a
kspield <141661179+kspield@users.noreply.github.com>
2025-07-29 07:23:07 +0200

    Update Garmin_Weight_Scrape.py
    put json file on home directory and reference there

--- Garmin_Weight_Scrape.py
+++ Garmin_Weight_Scrape.py
@@ -12,7 +12,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Path to your service account key (downloaded from Firebase Console)
-cred_path = os.path.expanduser("/home/pi/garmin-dashboard/fatboyslim-a061f-firebase-adminsdk-fbsvc-6ca353274a.json")
+cred_path = os.path.expanduser("~/fatboyslim-a061f-firebase-adminsdk-fbsvc-6ca353274a.json")
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
